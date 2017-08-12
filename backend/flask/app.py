180539c61dfb9784d345f0287221acd360d0b477
Arya Vohra <arya.vohra04@gmail.com>
2017-08-12 19:05:13 +0800

    Add GitHub API interfacing via PyGitHub & Google

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, g, session, redirect, url_for
 from flask_github import GitHub
+import requests
 
 app = Flask(__name__)
 app.config['GITHUB_CLIENT_ID'] = '24e70a9e5b30650ce068'
 app.config['GITHUB_CLIENT_SECRET'] = 'a797a49f99771bf1e4dda9d63f2360a46ac1a9f1'
+
+token = str
 
 @app.route('/login')
 @app.route('/')
@@ -13,32 +16,46 @@ def login():
 github = GitHub(app)
 
 @app.route('/callback', methods=['GET'])
-def authorized():
-    return request.args.get('code')
+@github.authorized_handler
+def authorized(oauth_token):
+    token = oauth_token
+    return oauth_token
 
-'''
-    next_url = request.args.get('next') or url_for('index')
-    if oauth_token is None:
-        flash("Authorization failed.")
-        return redirect(next_url)
+@github.access_token_getter
+def token_getter():
+    return token
 
-    user = User.query.filter_by(github_access_token=oauth_token).first()
-    if user is None:
-        user = User(oauth_token)
-        db_session.add(user)
+    # next_url = request.args.get('next') or url_for('index')
+    # if access_token is None:
+    #     return redirect(next_url)
 
-    user.github_access_token = oauth_token
-    db_session.commit()
-    return redirect(next_url)
+    # user = User.query.filter_by(github_access_token=access_token).first()
+    # if user is None:
+    # user.github_access_token = access_token
 
-def authorized():
-    print(token)
-    next_url = 'localhost:5000/index'
-    return ''''''
-'''
+    # session['user_id'] = user.id
+    # return redirect(next_url)
+# @github.authorized_handler
+# def authorized(shit):
+#     global token
+#     token = request.args.get('code')
+#     return token
+
+# @github.access_token_getter
+# def token_getter():
+#     user = g.user
+#     if user is not None:
+#         return user.github_access_token
 
 @app.route('/repo')
 def repo():
-    repo_dict = github.get('repos/cenkalti/github-flask')
-    return str(repo_dict)
+    session = requests.Session()
+    session.headers['Authorization'] = 'token %s' % token
+    base = 'https://api.github.com/'
+    #resp = requests.get(base+'/user/repos')
+    resp = session.post('https://api.github.com/user')
+    return resp.text
+
 app.run(debug=True, host="0.0.0.0", threaded=True)
+
+
