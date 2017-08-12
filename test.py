10f9de913f2b9060d4fbac1dcb4fc95526e85a30
Taichi Kato <taichikato0909@gmail.com>
2017-08-13 01:05:00 +0800

    Import parser now working

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 from googleapiclient import discovery
 from os.path import splitext
 from oauth2client.client import GoogleCredentials
+import base64
 
 # ARYAS OAUTH TOKEN
 g = Github("46658ee63b136199658e3a201ccbddec743bda00")
@@ -35,21 +36,38 @@ def retrieve_commit_messages(repo_object_arg):
 
 def retrieve_commit_files(repo_object_arg):
 	commit_filename_array = []
+	commit_filename_dict = {}
+	counter = 0
 	for x in repo_object_arg.get_commits(author=user):
 		#print(type(x))
 		#return False
 		#commit_filename_array.append(x.files.filename)
-		print(x.files)
-		print(x.files.contents_url)
-	return commit_filename_array
+		for file in x.files:
+			print("counter: ", counter)
+			if counter > 9:
+				return commit_filename_dict
+			# print(
+			# 	base64.b64decode(
+			# 		repo_object_arg.get_file_contents(file.filename).content
+			# 	)
+			# )
+			commit_filename_dict[file.filename] = base64.b64decode(
+					repo_object_arg.get_file_contents(file.filename).content
+				)
+			#print(x.files.contents_url)
+			counter += 1
+	return commit_filename_dict
 
 repo_obj, array = retrieve_repos(user)
-print(retrieve_commit_files(repo_obj[4]))
+files = retrieve_commit_files(repo_obj[3])
+
+print(repo_obj[3].name)
+
+#print(array)
 
 string = ""
-
 # ASSUMES THEY'VE CHOSEN A REPO AND WE KNOW WHAT INDEX IT IS
-for x in retrieve_commit_messages(repo_obj[4]):
+for x in retrieve_commit_messages(repo_obj[3]):
 	#print(x)
 	string += x
 
@@ -93,11 +111,12 @@ bant = "Fix some missing requires, globals, whitespace Change some http scripts 
 # PSEUDO CODE FOR THE IMPORT PARSING
 # LIBRARIES ARE KEYWORDS
 
-def get_library(codefile):
+def get_library(codefile, codevalue):
 	keywords = []
 
 	imports = {
 		'.py':'import',
+		'.pyx':'cimport',
 		'.swift':'import',
 		'.c':'include',
 		'.cpp':'include',
@@ -107,30 +126,61 @@ def get_library(codefile):
 		'.js':'require'
 	}
 
-	with open(codefile) as code:
-		_, ext = splitext(codefile)
-		if ext in imports.keys():
-			search_statement = imports[ext]
-			counter = 0
-			for line in code:
-				if counter < 3:
-					if search_statement in line:
-						if ext == '.py':
-							line = line.strip('\n')
-							line = line.replace(search_statement, '')
+	_, ext = splitext(codefile)
+	if ext in imports.keys():
+		#print(ext)
+		search_statement = imports[ext]
+		counter = 0
+		for line in codevalue.split('\\n'):
+				if search_statement in line:
+					if ext == '.py' or '.pyx':
+						if len(line.split(' ')) > 2:
+							line = line.replace(search_statement, '')	
 							if 'from' in line:
-								line = line.split()[1]
+								# from x import y
+								line = line.split(' ')[1]
+							elif 'as' in line:
+								# import x as y
+								line = line.split(' ')[1]
+						elif len(line.split(' ')) == 2:
+							# import x	
+							line = line.replace(search_statement, '')
+							line = line.strip(' ')
+
+					if '.' in line:
+						line = line.split('.')[0]
+					if line not in keywords:
 						keywords.append(line)
-					else:
-						counter += 1
-				else:
-					pass
+					#keywords.append(next(line, None))
+
+
+	# with open(codefile) as code:
+	# 	_, ext = splitext(codefile)
+	# 	if ext in imports.keys():
+	# 		search_statement = imports[ext]
+	# 		counter = 0
+	# 		for line in code:
+	# 			if counter < 3:
+	# 				if search_statement in line:
+	# 					if ext == '.py':
+	# 						line = line.strip('\n')
+	# 						line = line.replace(search_statement, '')
+	# 						if 'from' in line:
+	# 							line = line.split()[1]
+	# 					keywords.append(line)
+	# 				else:
+	# 					counter += 1
+	# 			else:
+	# 				pass
 
 	return keywords
 
 #print(get_library('/Users/aryavohra/questo-backend/app.py'))
 
+#print(files)
 
+for x in files:
+	print(get_library(str(x), str(files[x])))
 
 
 
